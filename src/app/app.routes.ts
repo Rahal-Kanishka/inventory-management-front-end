819375415e94f56c832306ec803a554bf65188a2
Rahal Danthanarayana <rahalncm@gmail.com>
2024-10-22 12:31:50 -0400

    added dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default route
+  { path: 'dashboard', component: DashboardComponent },
+
+];
