9da250c3357354b5eb94bed7a89ac051292d23c7
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-04 01:18:49 -0500

    project due date added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  submissionDate = new Date('December 11, 2024');
+  currentDate = Date.now();
+  day = 24*60*60*1000
+  result = Math.round((this.submissionDate.getTime() - this.currentDate)/(this.day))
   title = 'inventory-management';
 }
