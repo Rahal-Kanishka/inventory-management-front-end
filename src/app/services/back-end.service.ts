e5662912880e8ed1c180fa91f5f355ca7f41d134
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-05 17:05:21 -0500

    separate service added for back-end

---
+++ src/app/services/back-end.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { ColDef } from "ag-grid-community";
+
+const API_URL = 'http://localhost:8000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackEndService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+    getRequest(getPath: string): Observable<any> {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + sessionStorage.getItem('auth_token')
+      });
+      return this.http.get<any>(API_URL + getPath, { headers });
+    }
+
+
+    putRequest(putPath: string): Observable<any> {
+      const headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + sessionStorage.getItem('auth_token')
+      });
+      return this.http.put<any>(API_URL + putPath, { headers });
+    }
+
+    postRequest(saveData: any, postPath: string): Observable<any> {
+
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json'
+        })
+      };
+
+      return new Observable((subscriber) => {
+        this.http.post<any>(API_URL + postPath, saveData, httpOptions).subscribe(
+          () => {
+            return subscriber.next(true)
+          }, error => {
+            return subscriber.next(false)
+          }
+        );
+      });
+
+
+
+    }
+
+
+
+}
