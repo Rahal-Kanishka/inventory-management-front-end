13fb297c5d1f071df80c532935c38261f932abc2
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-05 15:43:08 -0500

    moved content back to app component to make routing easier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AgGridModule} from "ag-grid-angular";
-import {AppComponent} from "./app.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ToastrModule, ToastrService} from "ngx-toastr";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {provideRoutes, RouterModule, RouterOutlet, Routes} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgGridModule } from "ag-grid-angular";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { RouterLink, RouterModule, RouterOutlet, Routes } from "@angular/router";
+import { IngredientListComponent } from "./ingredient-list/ingredient-list.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
 
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // Default route
-  {path: 'dashboard', component: DashboardComponent}
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default route
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'ingredient-list', component: IngredientListComponent },
+  { path: '**', component: NotFoundComponent },
+
+
 ]
 
 @NgModule({
