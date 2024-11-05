14c646b473d0585c1c365fe252e6309d441e2cd3
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-05 15:44:15 -0500

    change the directory of ingredient list

--- src/app/ingredient-list/ingredient-list.component.ts
+++ src/app/ingredient-list/ingredient-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ingredient-list',
+  standalone: true,
+  imports: [],
   templateUrl: './ingredient-list.component.html',
   styleUrl: './ingredient-list.component.css'
 })
