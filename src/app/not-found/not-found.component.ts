da2379a88303e9d96556e491fefa1fb546a30693
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-09 21:25:54 -0500

    move out of stand-alone components

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  standalone: true,
-  imports: [],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
