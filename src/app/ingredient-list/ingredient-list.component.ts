0c1e9ab1c14eb10a4cc4e40f52e4084835956c6d
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-10 01:45:35 -0500

    add ingredient form and post request implemented

--- src/app/ingredient-list/ingredient-list.component.ts
+++ src/app/ingredient-list/ingredient-list.component.ts
@@ -3,8 +3,12 @@ import { AgGridAngular } from "ag-grid-angular";
 import { Observable, take } from "rxjs";
 import { BackEndService } from "../services/back-end.service";
 import { ColDef, GridOptions } from "ag-grid-community";
+import { Modal } from 'bootstrap';
+import { FormControl, FormGroup } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
 
 const GET_INGREDIENTS = '/ingredient/all'
+const ADD_INGREDIENTS = '/ingredient/add'
 @Component({
   selector: 'app-ingredient-list',
   templateUrl: './ingredient-list.component.html',
@@ -12,15 +16,37 @@ const GET_INGREDIENTS = '/ingredient/all'
 })
 export class IngredientListComponent {
 
-  public ingredientList = []
+  public ingredientList: any[] = []
 
   columnDefs: ColDef[] = [
     { field: 'id' },
     { field: 'name' },
-    { field: 'quantity' }
+    { field: 'description' },
+    { field: 'currentQuantity' }
   ];
-  constructor(private backEndService: BackEndService) {
-    this.getIngredientData()
+
+  ingredientForm: FormGroup;
+  constructor(private backEndService: BackEndService, private toastrService: ToastrService) {
+    this.getIngredientData();
+    document.getElementById('exampleModal')?.addEventListener('shown.bs.modal', () => {
+      (document.getElementById('myInput') as HTMLInputElement)?.focus();
+      console.log('haha')
+    });
+
+    this.ingredientForm = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl('')
+    });
+  }
+
+  openModal(): void {
+    const modalElement = document.getElementById('exampleModal');
+    // Ensure the element exists and cast it as an HTMLElement
+    if (modalElement) {
+      // Create a new Bootstrap Modal instance
+      const myModal = new Modal(modalElement);
+      myModal.show();  // Opens the modal
+    }
   }
 
   public gridOptions: GridOptions = {
@@ -43,6 +69,20 @@ export class IngredientListComponent {
       .subscribe(
         {next: (response) => this.ingredientList = response}
     )
+  }
+
+  public saveIngredientData() {
+    this.backEndService.postRequest(this.ingredientForm?.value, ADD_INGREDIENTS)
+      .pipe(take(1))
+      .subscribe(
+        {
+          next: (response) => {
+            // to trigger array update into the grid
+            this.ingredientList = [...this.ingredientList, response];
+            this.toastrService.success(response?.name + ' is saved','Successfully added')
+          },
+          error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in saving data',)}
+      )
   }
 
 }
