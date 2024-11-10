6f9af9dc69cf492fc230aa2bcf0bf009e67cc41d
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-09 21:25:45 -0500

    added common grid component to ingredient component

--- src/app/ingredient-list/ingredient-list.component.ts
+++ src/app/ingredient-list/ingredient-list.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from "ag-grid-angular";
+import { Observable, take } from "rxjs";
+import { BackEndService } from "../services/back-end.service";
+import { ColDef, GridOptions } from "ag-grid-community";
 
+const GET_INGREDIENTS = '/ingredient/all'
 @Component({
   selector: 'app-ingredient-list',
-  standalone: true,
-  imports: [],
   templateUrl: './ingredient-list.component.html',
   styleUrl: './ingredient-list.component.css'
 })
 export class IngredientListComponent {
+
+  public ingredientList = []
+
+  columnDefs: ColDef[] = [
+    { field: 'id' },
+    { field: 'name' },
+    { field: 'quantity' }
+  ];
+  constructor(private backEndService: BackEndService) {
+    this.getIngredientData()
+  }
+
+  public gridOptions: GridOptions = {
+    rowHeight: 45,
+    autoSizeStrategy: {
+      type: 'fitGridWidth'
+    },
+    getRowStyle: params => {
+      if (params && params.node.rowIndex && params.node.rowIndex % 2 === 1) {
+        return { background: 'gray' };
+      } else {
+        return { background: 'white' };
+      }
+    }
+  }
+
+  public getIngredientData() {
+    this.backEndService.getRequest(GET_INGREDIENTS)
+      .pipe(take(1))
+      .subscribe(
+        {next: (response) => this.ingredientList = response}
+    )
+  }
 
 }
