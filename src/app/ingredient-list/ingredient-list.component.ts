33553e92e3466ed9b26686639e86a93b23cd6a59
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-11 14:09:55 -0500

    moving out the template code to separate component

--- src/app/ingredient-list/ingredient-list.component.ts
+++ src/app/ingredient-list/ingredient-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AgGridAngular } from "ag-grid-angular";
 import { Observable, take } from "rxjs";
 import { BackEndService } from "../services/back-end.service";
@@ -6,6 +6,7 @@ import { ColDef, GridOptions } from "ag-grid-community";
 import { Modal } from 'bootstrap';
 import { FormControl, FormGroup } from "@angular/forms";
 import { ToastrService } from "ngx-toastr";
+import { IngredientAddComponent } from "../ingredient-add/ingredient-add.component";
 
 const GET_INGREDIENTS = '/ingredient/all'
 const ADD_INGREDIENTS = '/ingredient/add'
@@ -15,6 +16,12 @@ const ADD_INGREDIENTS = '/ingredient/add'
   styleUrl: './ingredient-list.component.css'
 })
 export class IngredientListComponent {
+
+  @ViewChild(IngredientAddComponent) ingredientAddComponent!: IngredientAddComponent;
+
+  openModal() {
+    this.ingredientAddComponent.openModal();
+  }
 
   public ingredientList: any[] = []
 
@@ -39,16 +46,6 @@ export class IngredientListComponent {
     });
   }
 
-  openModal(): void {
-    const modalElement = document.getElementById('exampleModal');
-    // Ensure the element exists and cast it as an HTMLElement
-    if (modalElement) {
-      // Create a new Bootstrap Modal instance
-      const myModal = new Modal(modalElement);
-      myModal.show();  // Opens the modal
-    }
-  }
-
   public gridOptions: GridOptions = {
     rowHeight: 45,
     autoSizeStrategy: {
@@ -71,18 +68,7 @@ export class IngredientListComponent {
     )
   }
 
-  public saveIngredientData() {
-    this.backEndService.postRequest(this.ingredientForm?.value, ADD_INGREDIENTS)
-      .pipe(take(1))
-      .subscribe(
-        {
-          next: (response) => {
-            // to trigger array update into the grid
-            this.ingredientList = [...this.ingredientList, response];
-            this.toastrService.success(response?.name + ' is saved','Successfully added')
-          },
-          error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in saving data',)}
-      )
+  onModalClosed(addedRecord: any) {
+    this.ingredientList = [...this.ingredientList, addedRecord];
   }
-
 }
