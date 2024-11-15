b51f10cc56f4ef3795fbb165d43f112b25945755
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-15 09:17:11 -0500

    location edit component created

---
+++ src/app/location-edit/location-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-edit',
+  templateUrl: './location-edit.component.html',
+  styleUrl: './location-edit.component.css'
+})
+export class LocationEditComponent {
+
+}
