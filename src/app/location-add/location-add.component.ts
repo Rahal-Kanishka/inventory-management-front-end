fcd2c47ae63397f987a7acea4b95f6fa023b084c
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-15 09:17:01 -0500

    location add component created

---
+++ src/app/location-add/location-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-add',
+  templateUrl: './location-add.component.html',
+  styleUrl: './location-add.component.css'
+})
+export class LocationAddComponent {
+
+}
