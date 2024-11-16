3f34b1eb1196d1417ce404bc3b5a00e1f1500320
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-16 03:16:22 -0500

    implemented location update modal

--- src/app/location-edit/location-edit.component.ts
+++ src/app/location-edit/location-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Modal } from "bootstrap";
+import { BackEndService } from "../services/back-end.service";
+import { ToastrService } from "ngx-toastr";
+import { take } from "rxjs";
+
+const UPDATE_LOCATION = '/location/update/'
 
 @Component({
   selector: 'app-location-edit',
@@ -7,8 +14,54 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class LocationEditComponent {
   @Output() modalClosed = new EventEmitter<any>();
+  @ViewChild('locationEditModel', { static: true }) modalElement!: ElementRef;
+  locationForm: FormGroup;
+  addedValue = null;
+  public locationData: any = null;
+  private myModal!: Modal;
 
-  openEditModal(data: any) {
+  constructor(
+    private backEndService: BackEndService,
+    private toastrService: ToastrService) {
+    this.locationForm = new FormGroup({
+      id: new FormControl({value: this.locationData?.id, disabled: true}, [Validators.required]),
+      name: new FormControl(this.locationData?.name, [Validators.required]),
+      address: new FormControl(this.locationData?.address, [Validators.required])
+    });
+  }
 
+  openEditModal(dataFromParentComponent: any) {
+    if(dataFromParentComponent){
+      this.locationForm.patchValue(dataFromParentComponent)
+    }
+    this.myModal = new Modal(this.modalElement.nativeElement);
+    this.myModal.show();
+  }
+
+  closeModal() {
+    this.locationForm.reset()
+    // Hide the modal
+    this.myModal.hide();
+
+  }
+
+  updateLocationData() {
+    if (!this.locationForm.invalid) {
+      this.backEndService.putRequest(this.locationForm?.getRawValue(), UPDATE_LOCATION + this.locationForm.getRawValue()?.id)
+        .pipe(take(1))
+        .subscribe(
+          {
+            next: (response) => {
+              // to trigger array update into the grid
+              this.addedValue = response;
+              this.toastrService.success(response?.name + ' is saved', 'Successfully added')
+              // Emit the data
+              this.modalClosed.emit(this.addedValue);
+              this.closeModal();
+            },
+            error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in saving data',)
+          }
+        )
+    }
   }
 }
