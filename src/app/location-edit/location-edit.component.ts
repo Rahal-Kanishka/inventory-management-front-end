391425c3c9559d9747ae200865010a158117f23b
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-17 22:51:30 -0500

    added changes to filter user by name , assign user to location and remove a user from a assigned location

--- src/app/location-edit/location-edit.component.ts
+++ src/app/location-edit/location-edit.component.ts
@@ -5,8 +5,11 @@ import { BackEndService } from "../services/back-end.service";
 import { ToastrService } from "ngx-toastr";
 import { take } from "rxjs";
 
-const ASSIGN_USERS = '/user/'
+const GET_ASSIGN_USERS = '/user/location/'
 const UPDATE_LOCATION = '/location/update/'
+const SEARCH_USER = '/user/'
+const ASSIGN_USER = '/location/assign_user/'
+const REMOVE_USER_FROM_LOCATION = '/location/remove_user/'
 
 @Component({
   selector: 'app-location-edit',
@@ -15,12 +18,14 @@ const UPDATE_LOCATION = '/location/update/'
 })
 export class LocationEditComponent {
   @Output() modalClosed = new EventEmitter<any>();
+  @Output() userAddOrRemove = new EventEmitter<any>();
   @ViewChild('locationEditModel', { static: true }) modalElement!: ElementRef;
   locationForm: FormGroup;
   addedValue = null;
   public locationData: any = null;
   private myModal!: Modal;
   public assignedUsersList: any[] = []
+  public suggestedUsersList: any[] = []
 
   constructor(
     private backEndService: BackEndService,
@@ -70,7 +75,7 @@ export class LocationEditComponent {
   }
 
   private getAssignUsers(){
-    this.backEndService.getRequest( ASSIGN_USERS + this.locationData?.id)
+    this.backEndService.getRequest( GET_ASSIGN_USERS + this.locationData?.id)
       .pipe(take(1))
       .subscribe(
         {
@@ -83,11 +88,59 @@ export class LocationEditComponent {
       )
   }
 
-  assignUsersToLocation() {
-
+  public removeAssignedUser(userID: any) {
+    this.backEndService.deleteRequest( REMOVE_USER_FROM_LOCATION + userID + '/' + this.locationData?.id)
+      .pipe(take(1))
+      .subscribe(
+        {
+          next: (response) => {
+            this.assignedUsersList = response
+            this.toastrService.success('Successfully removed User', 'Success')
+            this.emitEventToParentComponent();
+          },
+          error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in retrieving users data',)
+        }
+      )
   }
 
-  removeAssignedUser(userID: any) {
+  public findUsersByName(input: any){
+    let inputElement: any = document.getElementById('name-input')
+    console.log('button: ', inputElement?.value)
+    if (inputElement && inputElement?.value) {
+      this.backEndService.getRequest(SEARCH_USER + inputElement?.value)
+        .pipe(take(1))
+        .subscribe(
+          {
+            next: (response) => {
+              this.suggestedUsersList = response
+            },
+            error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in retrieving users data')
+          }
+        )
+    }
+  }
 
+  public assignUser(userID: any) {
+    this.backEndService.putRequest(this.locationForm?.getRawValue(), ASSIGN_USER + userID + '/' + this.locationData?.id)
+      .pipe(take(1))
+      .subscribe(
+        {
+          next: (response: any) => {
+            // add to the assigned list
+            this.assignedUsersList = response
+            this.toastrService.success('Successfully added', 'Success')
+            this.emitEventToParentComponent();
+          },
+          error: (v) => this.toastrService.error(v?.toString(), 'Error occurred in saving data',)
+        }
+      )
+  }
+
+  private emitEventToParentComponent() {
+    this.userAddOrRemove.emit(
+      {
+        location_id: this.locationData?.id,
+        users: this.assignedUsersList
+      });
   }
 }
