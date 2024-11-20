0c861cd9dd8397f60cf3137c2a0342ffa544f0d1
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-20 09:35:39 -0500

    changes to show user list and grid formatting

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BackEndService } from "../services/back-end.service";
+import { ToastrService } from "ngx-toastr";
+import { take } from "rxjs";
+import { ColDef } from "ag-grid-community";
+import { EditCellComponent } from "../common/edit-cell/edit-cell.component";
+import { UserAddComponent } from "../user-add/user-add.component";
+import { UserEditComponent } from "../user-edit/user-edit.component";
+
+const GET_USERS = '/user/all'
 
 @Component({
   selector: 'app-user-list',
@@ -7,4 +16,91 @@ import { Component } from '@angular/core';
 })
 export class UserListComponent {
 
+  @ViewChild(UserAddComponent) userAddComponent!: UserAddComponent;
+  @ViewChild(UserEditComponent) userEditComponent!: UserEditComponent;
+
+  public userList: any[] = []
+
+  context = { componentParent: this };
+
+  columnDefs: ColDef[] = [
+    { field: 'id',
+      maxWidth: 100,
+      headerName: 'ID'
+    },
+    { field: 'name',
+      filter: true
+    },
+    {
+      field: 'type',
+      maxWidth: 120,
+      valueFormatter: (params: any) => (!params && !params.data && !params.data.userType
+        && params.data?.userType?.name) ? 'Not Assigned' : params.data?.userType?.name.toUpperCase()
+    },
+    { field: 'contactNo',
+      maxWidth: 120,
+    },
+    { field: 'email' },
+    {
+      field: 'createdOn',
+      minWidth: 180,
+      valueFormatter: (params: any) => {
+        if (params.data && params.data.createdOn) {
+          const date = new Date(params.data.createdOn);
+          return date.toLocaleString(); // Format the date as 'MM/DD/YYYY, HH:mm:ss'
+        }
+        return 'No Date Provided'
+      }
+    },
+
+    {
+      field: '',
+      cellRenderer: EditCellComponent
+    }
+  ];
+
+  constructor(private backEndService: BackEndService, private toastrService: ToastrService) {
+    this.getUserListData();
+  }
+
+  public getUserListData() {
+    this.backEndService.getRequest(GET_USERS)
+      .pipe(take(1))
+      .subscribe(
+        { next: (response) => this.userList = response }
+      )
+  }
+
+  public openAddModal() {
+    this.userAddComponent.openModal();
+  }
+
+  openModalFromCellRenderer(data: any) {
+    // Pass the data to the modal component
+    this.userEditComponent.openEditModal(data);
+  }
+
+  onUserAdded($event: any) {
+    console.log('location add: ', $event)
+    // make it compatible to the grid data
+    this.userList = [...this.userList, $event]
+  }
+
+  testNewRecord(){
+    console.log(this.userList[0])
+    this.userList = [...this.userList, this.userList[0]]
+  }
+
+  onUserEdited(updatedUser: any) {
+    let tempList = []
+    for (let ingredient of this.userList){
+      if (updatedUser.id != ingredient.id){
+        tempList.push(ingredient)
+      } else {
+        tempList.push(updatedUser)
+      }
+    }
+    // to trigger grid update
+    this.userList = [...tempList];
+  }
 }
