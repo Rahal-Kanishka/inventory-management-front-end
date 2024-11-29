5b8569746b97e3458f77feb3f7900606fe307933
Rahal Danthanarayana <rahalncm@gmail.com>
2024-11-28 22:29:14 -0500

    product add component created

---
+++ src/app/product/product-add/product-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrl: './product-add.component.css'
+})
+export class ProductAddComponent {
+
+}
